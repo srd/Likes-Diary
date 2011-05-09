7a25cc63e6c076d2ccc9e71a4d495a4b6eb89fe4
unknown <casino@.(none)>
2011-05-09 16:25:39 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likesdiary::Application.config.secret_token = '4959a5163e08e95cd4e24deca5638ec9308b93b9380531558735b2e73c6df647fb61dce9ba8d013dc67c5074325daa062bd17bc54bcbb6c2471a3aff488319e0'
