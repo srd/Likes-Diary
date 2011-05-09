226af440cecfcb2f9059e069bcdd270af6398710
unknown <casino@.(none)>
2011-05-09 17:43:47 +0530

    homepage updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def home
+	@user_session = UserSession.new
+	@user = User.new
 	@title = "Home"
 	if !current_user
 	  return
 	end
-	redirect_to users_path
+	redirect_to users_path#need to make this homepage
   end
 
   def contact
